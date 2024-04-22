45b3ff16eb2192e34951bc9c2edfd0e1f3e91bf5
Sırac Yücel <144338189+siracyucel@users.noreply.github.com>
2024-04-22 03:46:45 +0300

    Add files via upload

---
+++ lua/xiax/remap.lua
@@ -0,0 +1,15 @@
+vim.g.mapleader = " "
+vim.keymap.set("n", "<leader>e", vim.cmd.Ex)
+
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
+vim.keymap.set("n", "<Tab>", "<Cmd>BufferNext<CR>")
+vim.keymap.set("n", "<leader>x", "<cmd>bw!<CR>")
+
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
