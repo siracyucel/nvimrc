9648670a61a53f405f8d3d26f9457d494423b525
Sırac Yücel <144338189+siracyucel@users.noreply.github.com>
2024-07-12 14:15:59 +0300

    Update remap.lua

--- lua/xiax/remap.lua
+++ lua/xiax/remap.lua
@@ -7,9 +7,8 @@ vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
 
-vim.keymap.set("n", "<Tab>", "<Cmd>BufferNext<CR>")
-vim.keymap.set("n", "<leader>x", "<cmd>bw!<CR>")
+vim.keymap.set("n", "<leader>x", "<Cmd>bdelete<CR>")
+vim.keymap.set("n", "<Tab>", "<Cmd>BufferLineCycleNext<CR>")
 
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
-
